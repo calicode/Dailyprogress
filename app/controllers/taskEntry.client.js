53b1305bc0550d93d3d1df7b6a1a5732e7d46ee0
Andrew Schmidthuber <aschmidthuber@gmail.com>
2016-05-05 19:47:14 -0500

    timer cleanup, reading task text, some notes about how i will pull tags and post to server

--- app/controllers/taskEntry.client.js
+++ app/controllers/taskEntry.client.js
@@ -1,12 +1,20 @@
 (function(){
-
+'use strict';
 
 var timerState = {
 started:false,
 sessionTime:0,
-startTime:0
+startTime:0,
+
+getMinutes: function(){
+	return Math.ceil(this.sessionTime / 60); 	
+	},
+resetState: function(){
+	this.started = false;
+	this.sessionTime = 0;
+	this.startTime = 0;
+	}
 };
-console.log("I have loazded");
 
 
 
@@ -15,15 +23,17 @@ document.getElementById("butStartTimer").onclick = startTimer;
 document.getElementById("butEndWork").onclick = endWork;
 
 function startTimer() {
+
 if (timerState.started){
 	
 	console.log("Stopping timer at ", new Date());
 	timerState.started = false;
 	let timeTemp = (new Date().getTime()) - (timerState.startTime);
-	timerState.sessionTime = timerState.sessionTime + ( (timeTemp / 600) /  60 );
+	timerState.sessionTime = +timerState.sessionTime + ( (timeTemp / 600) );
 	timerState.sessionTime = +timerState.sessionTime.toFixed();
 	timerState.startTime = 0; 
-	console.log("sessionTime is now ", timerState.sessionTime, " minutes");
+	console.log(timerState.sessionTime);
+	console.log("sessionTime is now ", timerState.getMinutes(), " minutes");
 
 }
 
@@ -42,7 +52,26 @@ function endWork() {
 info into database after validating it. 
 
 */
-console.log("end button clicked good job");
+if (!timerState.started){
+
+	console.log("Session time was ", timerState.getMinutes()," minutes ");
+	console.log(timerState.sessionTime, "seconds");
+	timerState.resetState();
+	let formVal = document.getElementById("taskNotes");
+	console.log(formVal.value);
+}
+else if (timerState.started){
+	console.log("Session time was ", timerState.getMinutes()," minutes ");
+	console.log(timerState.sessionTime, "seconds");
+	let timeTemp = (new Date().getTime()) - (timerState.startTime);
+	timerState.sessionTime = +timerState.sessionTime + ( (timeTemp / 600) );
+	timerState.sessionTime = +timerState.sessionTime.toFixed();
+	timerState.resetState();
+	let formVal = document.getElementById("taskNotes");
+	console.log(formVal.value);
+
+}
+
 
 }
 
