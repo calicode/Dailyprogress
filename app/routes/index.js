67560e2a57857fddbc15236b8ab7a28506148ee6
calicode <andrew.schmidthuber@gmail.com>
2016-06-20 23:23:53 +0000

    Added function so only I can save tasks

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,11 +4,19 @@ var path = process.cwd();
 var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
 var TaskHandler = require(path + '/app/controllers/taskHandler.server.js');
 var bodyparser = require('body-parser');
- 
 
-module.exports = function (app, passport) {
 
-	function isLoggedIn (req, res, next) {
+module.exports = function(app, passport) {
+	function isCalicode(req, res, next) {
+		if (req.isAuthenticated() && req.github.id === 16291386) {
+			return next();
+		} else {
+			res.redirect('/login');
+
+		}
+	}
+
+	function isLoggedIn(req, res, next) {
 		if (req.isAuthenticated()) {
 			return next();
 		} else {
@@ -21,28 +29,28 @@ module.exports = function (app, passport) {
 	app.use(bodyparser.json());
 
 	app.route('/')
-		.get(isLoggedIn, function (req, res) {
+		.get(isLoggedIn, function(req, res) {
 			res.sendFile(path + '/public/index.html');
 		});
 
 	app.route('/login')
-		.get(function (req, res) {
+		.get(function(req, res) {
 			res.sendFile(path + '/public/login.html');
 		});
 
 	app.route('/logout')
-		.get(function (req, res) {
+		.get(function(req, res) {
 			req.logout();
 			res.redirect('/login');
 		});
 
 	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
+		.get(isLoggedIn, function(req, res) {
 			res.sendFile(path + '/public/profile.html');
 		});
 
 	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
+		.get(isLoggedIn, function(req, res) {
 			res.json(req.user.github);
 		});
 
@@ -63,17 +71,17 @@ module.exports = function (app, passport) {
 
 
 	app.route('/schedule')
-		.get(isLoggedIn,function(req,res){
-			
+		.get(isLoggedIn, function(req, res) {
+
 			res.sendFile(path + '/public/schedule.html');
 
 		})
-		.post(isLoggedIn,taskHandler.logtask);
+		.post(isLoggedIn, isCalicode, taskHandler.logtask);
 
-		app.route('/tasks')
-		.get(isLoggedIn,taskHandler.getTasks);
+	app.route('/tasks')
+		.get(isLoggedIn, taskHandler.getTasks);
 
-		app.route('/weektotal')
+	app.route('/weektotal')
 		.get(isLoggedIn, taskHandler.getWeeklyTotal);
 
 
