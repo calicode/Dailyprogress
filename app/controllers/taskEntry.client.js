605670ab5a7b09e9b61f3bea1e6d247982e9ffd9
calicode <andrew.schmidthuber@gmail.com>
2016-06-21 00:12:39 +0000

    fixed weekly time pull, wasn't using new Date properly. Trying to make test data now in mongo. Need to find a better way than entering console commands

--- app/controllers/taskEntry.client.js
+++ app/controllers/taskEntry.client.js
@@ -1,74 +1,72 @@
-(function(){
-'use strict';
-// 5-30-2016 this file is too large. put makepost/gettasks into ajax functions script. Put rest of timer stuff into timer controller. 
-const baseUrl = window.location.hostname + ":8080";
+(function() {
+	'use strict';
+	// 5-30-2016 this file is too large. put makepost/gettasks into ajax functions script. Put rest of timer stuff into timer controller. 
+	const baseUrl = window.location.hostname + ":8080";
 
-document.getElementById("butStartTimer").onclick = startTimer;
-document.getElementById("butEndWork").onclick = endWork;
-document.getElementById("butUpdateTasks").onclick = getWeeklyTotal;
-
-
-
-// sett up vue.js bindings. 
-
-var vm = new Vue({
-  el: '#task_list',
-  data: {
-    taskListResults: '',
-    weeklyTotal:getWeeklyTotal()
-  }
-});
-
-var vmTimeDisplay = new Vue ({
-el:'#schedule_top',
-data:{
-message: timerState.getDisplayMessage()
-}
-});
-
-
-function displayUpdate(){
-
-	vmTimeDisplay.message = timerState.getDisplayMessage();
-}
+	document.getElementById("butStartTimer").onclick = startTimer;
+	document.getElementById("butEndWork").onclick = endWork;
+	document.getElementById("butUpdateTasks").onclick = getWeeklyTotal;
 
 
 
+	// sett up vue.js bindings. 
+
+	var vm = new Vue({
+		el: '#task_list',
+		data: {
+			taskListResults: '',
+			weeklyTotal: getWeeklyTotal()
+		}
+	});
+
+	var vmTimeDisplay = new Vue({
+		el: '#schedule_top',
+		data: {
+			message: timerState.getDisplayMessage()
+		}
+	});
 
 
-function startTimer() {
+	function displayUpdate() {
 
-if (timerState.started){
+		vmTimeDisplay.message = timerState.getDisplayMessage();
+	}
 
-	window.clearInterval(timerState.displayUpdateInterval);
-	console.log("Stopping timer at ", new Date());
-	timerState.started = false;
-	timerState.endDate = new Date().getTime();
-	let timeTemp = timerState.endDate - timerState.startDate;
-	// + in front of the timerstate changes type into number. 
-	timerState.sessionTime = +timerState.sessionTime + (timeTemp / 600);
-	timerState.sessionTime = +timerState.sessionTime.toFixed();
-	timerState.startTime = 0; 
-// resets just the start time back to 0 so user can continue with the same session states. Re-evaluate this at time point. 
-// add display notification to indicate whats going on 'session time is 45 mins, timer stopped'. Make sure it restarts and adds
-// correctly. Write automated test to do this. 
 
-	
 
-}
 
-else if (!timerState.started) {	
-	console.log("Started");
 
-	timerState.started = true;
-	timerState.startDate = new Date().getTime();
-	displayUpdate();
-	
-}
+	function startTimer() {
 
-if (!timerState.displayUpdateInterval) { 
+		if (timerState.started) {
 
-	timerState.displayUpdateInterval = window.setInterval(displayUpdate,60000);	
+			window.clearInterval(timerState.displayUpdateInterval);
+			console.log("Stopping timer at ", new Date());
+			timerState.started = false;
+			timerState.endDate = new Date().getTime();
+			let timeTemp = timerState.endDate - timerState.startDate;
+			// + in front of the timerstate changes type into number. 
+			timerState.sessionTime = +timerState.sessionTime + (timeTemp / 600);
+			timerState.sessionTime = +timerState.sessionTime.toFixed();
+			timerState.startTime = 0;
+			// resets just the start time back to 0 so user can continue with the same session states. Re-evaluate this at time point. 
+			// add display notification to indicate whats going on 'session time is 45 mins, timer stopped'. Make sure it restarts and adds
+			// correctly. Write automated test to do this. 
+
+
+
+		} else if (!timerState.started) {
+			console.log("Started");
+
+			timerState.started = true;
+			timerState.startDate = new Date().getTime();
+			displayUpdate();
+
+		}
+
+		if (!timerState.displayUpdateInterval) {
+
+			timerState.displayUpdateInterval = window.setInterval(displayUpdate, 60000);
 		}
 
 	} // end startTimer()
@@ -76,86 +74,91 @@ if (!timerState.displayUpdateInterval) {
 
 
 
-function endWork() {
-/* once this is clicked I think we need to make a post request to the local host which server js will handle and put 
-info into database after validating it. 
+	function endWork() {
+		/* once this is clicked I think we need to make a post request to the local host which server js will handle and put 
+		info into database after validating it. 
 
-*/
-
-
-//handles sending data to the server 
+		*/
 
 
-function makePost(){
-vm.taskListResults = "makepost was clicked and updated the view";
-if (timerState.sessionTime && ( $('#taskNotes').val() ) ){   
-let minutes = +timerState.getMinutes();
-let taskInfo = {totalTime:minutes,
-		taskDetails:$('#taskNotes').val(),
-		startDate:timerState.startDate
-	
+		//handles sending data to the server 
 
 
-		};
-		$.post({
-			url: "http://"+baseUrl+"/schedule",
-			
-			statusCode:{
-			404:function(){alert("File not found");}
-
-			},
-			
-			type:'POST',
-			data:JSON.stringify(taskInfo),
-			contentType:"application/json"
-			
+		function makePost() {
+			vm.taskListResults = "makepost was clicked and updated the view";
+			if (timerState.sessionTime && ($('#taskNotes').val())) {
+				let minutes = +timerState.getMinutes();
+				let taskInfo = {
+					totalTime: minutes,
+					taskDetails: $('#taskNotes').val(),
+					startDate: timerState.startDate
 
 
 
-		})
-		
-		.done(function(){console.log("successfully  sent to server");
-timerState.resetState();
-	})
-		.fail(function(jqHxr,error,error_internal){console.log("Error sending to server, oh bother", error_internal);});
+				};
+				$.post({
+					url: "http://" + baseUrl + "/schedule",
 
-		
+					statusCode: {
+						404: function() {
+							alert("File not found");
+						}
 
-		} else if (!timerState.sessionTime) {
-			alert("Total session time is 0 minutes, you should probably do some work!");}
-		else if (!$('#taskNotes').val()) {
-			alert("Task notes are blank, what did you do today?");
+					},
+
+					type: 'POST',
+					data: JSON.stringify(taskInfo),
+					contentType: "application/json"
+
+
+
+
+				})
+
+				.done(function() {
+						console.log("successfully  sent to server");
+						timerState.resetState();
+					})
+					.fail(function(jqHxr, error, error_internal) {
+						console.log("Error sending to server, oh bother", error_internal);
+					});
+
+
+
+			} else if (!timerState.sessionTime) {
+				alert("Total session time is 0 minutes, you should probably do some work!");
+			} else if (!$('#taskNotes').val()) {
+				alert("Task notes are blank, what did you do today?");
+			}
+
 		}
 
-}
+
+		//handles different timer states. 
+		if (!timerState.started) {
+			makePost();
+			console.log("Session time was ", timerState.getMinutes(), " minutes ");
+			console.log(timerState.sessionTime, "seconds");
+
+			let formVal = document.getElementById("taskNotes");
 
 
-//handles different timer states. 
-if (!timerState.started){
-	makePost();
-	console.log("Session time was ", timerState.getMinutes()," minutes ");
-	console.log(timerState.sessionTime, "seconds");
-	
-	let formVal = document.getElementById("taskNotes");
-	
-	
-}
-else if (timerState.started){
-	
-	console.log("Session time was ", timerState.getMinutes()," minutes ");
-	console.log(timerState.sessionTime, "seconds");
-	timerState.endDate = new Date().getTime();
-	let timeTemp = (timerState.endDate) - (timerState.startDate);
-	
-	timerState.sessionTime = +timerState.sessionTime + ( (timeTemp / 600) );
-	timerState.sessionTime = +timerState.sessionTime.toFixed();
+		} else if (timerState.started) {
 
-	makePost();
+			console.log("Session time was ", timerState.getMinutes(), " minutes ");
+			console.log(timerState.sessionTime, "seconds");
+			timerState.endDate = new Date().getTime();
+			let timeTemp = (timerState.endDate) - (timerState.startDate);
 
-	let formVal = document.getElementById("taskNotes");
-	
+			timerState.sessionTime = +timerState.sessionTime + ((timeTemp / 600));
+			timerState.sessionTime = +timerState.sessionTime.toFixed();
+
+			makePost();
+
+			let formVal = document.getElementById("taskNotes");
+
+		}
 	}
-}
 
 
 
@@ -164,44 +167,50 @@ else if (timerState.started){
 
 
 
-function getWeeklyTotal(){
-	alert(location);
-	$.get("http://"+baseUrl+"/weektotal")
-	.fail(function(error){console.log("Error getting tasks", error); })
-	.done(function(data){
-		console.log("meow2" , data); 
-		
-	
-	} );
-	
-}
+	function getWeeklyTotal() {
+		alert(location);
+		$.get("http://" + baseUrl + "/weektotal")
+			.fail(function(error) {
+				console.log("Error getting tasks", error);
+			})
+			.done(function(data) {
+				console.log("meow2", data);
+
+
+			});
+
+	}
 
 
 
 
 
-function updateTasks(){
-	$.get("http://"+baseUrl+"/weektotal")
-	.fail(function(error){console.log("Error getting tasks", error); })
-	.done(function(data){
-		console.log("meow" , data); 
-		vm.taskListResults = data;
-	
-	} );
-	
-}
+	function updateTasks() {
+		$.get("http://" + baseUrl + "/weektotal")
+			.fail(function(error) {
+				console.log("Error getting tasks", error);
+			})
+			.done(function(data) {
+				console.log("meow", data);
+				vm.taskListResults = data;
+
+			});
+
+	}
 
 
-function getWeeklyTotal(){
-	$.get("http://"+baseUrl+"/weektotal")
-	.fail(function(error){console.log("Error getting tasks", error); })
-	.done(function(data){
-		console.log("meow" , data); 
-		vm.weeklyTotal = data[0].count;
-	
-	} );
-	
-}
+	function getWeeklyTotal() {
+		$.get("http://" + baseUrl + "/weektotal")
+			.fail(function(error) {
+				console.log("Error getting tasks", error);
+			})
+			.done(function(data) {
+				console.log("meow", data);
+				vm.weeklyTotal = data[0].count;
+
+			});
+
+	}
 
 
 
@@ -210,4 +219,4 @@ function getWeeklyTotal(){
 
 
 
-})(); 
+})();
--- app/controllers/taskHandler.server.js
+++ app/controllers/taskHandler.server.js
@@ -25,78 +25,102 @@ totalMins:Number,
 */
 
 
-function TaskHandler(){
-// 86400000 = 24 hours in miliseconds
-// 604800000 = 7 days in miliseconds
+function TaskHandler() {
+	// 86400000 = 24 hours in miliseconds
+	// 604800000 = 7 days in miliseconds
 
 
-//> db.tasks.aggregate([{"$group":{"_id":"$github.id","total":{"$sum":1}}}])
-//{ "_id" : "16291386", "total" : 32 } 
-/*
-b.tasks.aggregate([{"$match":{'github.id':'16291386','dates':{'$gt':'146352953000'}}},{$group:{_id:null,count:{$sum:1}}}])
-{ "_id" : null, "count" : 9 }
+	//> db.tasks.aggregate([{"$group":{"_id":"$github.id","total":{"$sum":1}}}])
+	//{ "_id" : "16291386", "total" : 32 } 
+	/*
+	db.tasks.aggregate([{"$match":{'github.id':16291386,'dates':{'$gt':146352953000}}},{$group:{_id:null,count:{$sum:1}}}])
+	{ "_id" : null, "count" : 9 }
+> db.tasks.aggregate([{'$match':{'startDate':{$gt:1465948182381,$lt:1466552982381}}},{$group:{_id:null,count:{$sum:'$totalTime'}}}])
+{ "_id" : null, "count" : 51 }
+	*/
+	this.getWeeklyTotal = function(req, res, next) {
 
-*/
-	this.getWeeklyTotal = function(req,res,next){
+			let endDate = new Date().getTime() + 86400000;
+			let startDate = new Date(endDate - 604800000).getTime();
+			console.log(endDate);
+			console.log(startDate);
 
-	let endDate = new Date().getTime() + 86400000;
-	let startDate = endDate - 604800000; 
+			Task.aggregate([{
+				$match: {
+					'github.id': req.user.github.id,
+					'startDate': {
+						$gt: startDate,
+						$lt: endDate
+					}
+				}
+			}, {
+				$group: {
+					_id: null,
+					count: {
+						$sum: '$totalTime'
+					}
+				}
 
+			}])
 
-	Task.aggregate([{
-		$match:
-		{'github.id':req.user.github.id,'startDate':{'$gt':startDate,'$lt':endDate} 
-		}} ,{$group:{_id:null,count:{$sum:'$totalTime'}}
-
-	}])
-
-			.exec(function (err, result) {
-				if (err) { throw err; }
+			.exec(function(err, result) {
+				if (err) {
+					throw err;
+				}
 				//res.json(result);
-    				console.log(result);
-    				res.json(result);	
-    				next();
-    			});
-  
-},
-
-	this.getTasks = function (req,res,next){
-		
-
-		//console.log(count);
-		Task
-			.findOne({ 'github.id': req.user.github.id,'totalTime':{$gt:0} }, { '_id': false })
-			.exec(function (err, result) {
-				if (err) { throw err; }
-				//res.json(result);
-    				res.json(result);
-return next();
-
+				console.log(result);
+				res.json(result);
+				next();
 			});
-			},
+
+		},
+
+		this.getTasks = function(req, res, next) {
 
 
-	this.logtask = function(req,res){
-		var newTask = new Task();
-		
-		console.log("request came in", req.body);
+			//console.log(count);
+			Task
+				.findOne({
+					'github.id': req.user.github.id,
+					'totalTime': {
+						$gt: 0
+					}
+				}, {
+					'_id': false
+				})
+				.exec(function(err, result) {
+					if (err) {
+						throw err;
+					}
+					//res.json(result);
+					res.json(result);
+					return next();
 
-
-
-		newTask.github.id = req.user.github.id;
-		newTask.totalTime =+req.body.totalTime;
-		newTask.taskText = req.body.taskDetails;
-		newTask.startDate = req.body.startDate;
-		newTask.save(function (err,result) {
-						if (err) {
-							throw err;
-						}
-						
-						res.end("Successfully saved");
-						console.log(result);
-				return (null, newTask);
 				});
-			
+		},
+
+
+		this.logtask = function(req, res) {
+			var newTask = new Task();
+
+			console.log("request came in", req.body);
+
+
+
+			newTask.github.id = req.user.github.id;
+			newTask.totalTime = +req.body.totalTime;
+			newTask.taskText = req.body.taskDetails;
+			newTask.startDate = req.body.startDate;
+			newTask.save(function(err, result) {
+				if (err) {
+					throw err;
+				}
+
+				res.end("Successfully saved");
+				console.log(result);
+				return (null, newTask);
+			});
+
 		}
 }
 
